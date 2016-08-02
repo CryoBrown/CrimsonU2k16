3c107f251761c66eeb01785b27298eef315803c7
Chris Brown <CryoBrown@users.noreply@github.com>
2016-08-02 09:41:09 -0500

    Classifier finished, spotify integration in development

--- actions.py
+++ actions.py
@@ -1,4 +1,6 @@
 import subprocess
+import applescript
+import time
 
 '''
 Causes the screen to sleep, which will also lock if your settings
@@ -6,3 +8,22 @@ are set appropriately (they should be anyway, set them that way)
 '''
 def sleep_mac_display():
     subprocess.call('pmset displaysleepnow', shell=True)
+
+
+script = applescript.AppleScript('''
+    on run
+        tell application "Spotify"
+            set currentID to id of current track as string
+
+            return currentID
+        end tell
+    end run
+''')
+
+def get_current_track():
+    return script.run()
+
+def save_current_track():
+    track = get_current_track()
+
+

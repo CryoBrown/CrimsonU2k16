750b840b494223a3db7bda1951dd8c9004b6ed7e
Malhotra, Dipit <MalhotrD@advisory.com>
2016-08-03 15:29:41 -0500

    Leap Motin Gesture:1

--- README.md
+++ README.md
@@ -6,7 +6,14 @@ OpenCV, whitespace sensitive snakes, and people waving their hands around
 This project relies on OpenCV and Python 2.7.
 We used the guide at http://www.pyimagesearch.com/2015/06/15/install-opencv-3-0-and-python-2-7-on-osx/ for installation, and it worked great.
 
+## Dependencies
+
+opencv
+
+numpy
+
+applescript (which depends on PyObjC)
+
 ## Acknowledgements
 
-Vipul Sharma's work was a good jumping off point
-https://github.com/vipul-sharma20/gesture-opencv/blob/master/gesture.py
+
--- actions.py
+++ actions.py
@@ -1,6 +1,8 @@
 import subprocess
 import applescript
 import time
+import spotipy
+import spotipy.util as util
 
 '''
 Causes the screen to sleep, which will also lock if your settings
@@ -9,6 +11,12 @@ are set appropriately (they should be anyway, set them that way)
 def sleep_mac_display():
     subprocess.call('pmset displaysleepnow', shell=True)
 
+
+'''
+Setup for spotify integration
+'''
+
+scope = 'user-library-modify'
 
 script = applescript.AppleScript('''
     on run
@@ -20,10 +28,38 @@ script = applescript.AppleScript('''
     end run
 ''')
 
+cbspotify = 'spotify:user:12157124018'
+
+scope = 'user-library-modify'
+
+sp = None
+
+def login_spotify(username):
+    token = util.prompt_for_user_token(username, scope)
+    global sp
+    if token:
+        sp = spotipy.Spotify(auth=token)
+    else:
+        print "Can't get token for", username
+
 def get_current_track():
     return script.run()
 
 def save_current_track():
+    if(sp == None):
+        print("fuck")
+        return False
     track = get_current_track()
+    sp.current_user_saved_tracks_add([track])
+    return True
+
+login_spotify(cbspotify)
+while True:
+    raw_input('Enter to save...')
+    save_current_track()
+
+
+
+
 
 

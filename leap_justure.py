35020c2a09f0bc0cf155fcae7a1bf1596dc3b0bf
Chris Brown <CryoBrown@users.noreply@github.com>
2016-08-03 16:42:14 -0500

    Implemented the rolling window and a tick rate for processing

--- leap_justure.py
+++ leap_justure.py
@@ -7,6 +7,13 @@ class JustureListener(Leap.Listener):
     bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
     state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
 
+    def __init__(self, framestore, ticks):
+        Leap.Listener.__init__(self)
+        self.l_win = JustureWindow(framestore)
+        self.r_win = JustureWindow(framestore)
+        self.tock = 0
+        self.ticks = ticks
+
     def on_init(self, controller):
         print "Initialized"
 
@@ -23,32 +30,84 @@ class JustureListener(Leap.Listener):
     def on_frame(self, controller):
         # Get the most recent frame and report some basic information
         frame = controller.frame()
+        #Check if this frame matters and if so use it
+        if self.tock == 0:
+            self.per_tick(frame)
+        self.tock = (self.tock+1)%self.ticks
+
+    def per_tick(self, frame):
+        #perform calculations per tick
 
         # Get hands
         for hand in frame.hands:
 
-            handType = "Left hand" if hand.is_left else "Right hand"
-
-            print "  %s, id %d, position: %s" % (
-                handType, hand.id, hand.palm_position)
-
-            fse = hand.fingers.extended()
-            # Get fingers
-            for finger in hand.fingers:
-
-                print "    %s finger, extended: %s" % (
-                    self.finger_names[finger.type],
-                    (finger in fse))
+            if hand.is_left:
+                print "LH"
+                win = self.l_win
+            else:
+                print "RH"
+                win = self.r_win
+            print "-------------"
+            win.push(hand)
+            print("Extended: %s", win.extended_tuple(hand))
+            print("Avg: %s", win.ext_average)
 
         if not (frame.hands.is_empty and frame.gestures().is_empty):
             print ""
 
-class JustureLogic():
-    queue = ()
+class JustureWindow():
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.id = -1
+
+    def reroll(self, capacity):
+        self.queue = []
+        self.space = capacity
+        self.ext_average = tuple([0.0, 0.0, 0.0, 0.0, 0.0])
+
+    #Add to the window (removing oldest if window is to size, updating average as needed)
+    def push(self, hand):
+        if(hand.id != self.id):
+            self.id = hand.id
+            self.reroll(self.capacity)
+        self.queue.append(hand)
+        if(self.space > 0):
+            self.space -= 1
+        else:
+            self.update_average(self.queue.pop(0), subtract=True)
+        self.update_average(hand)
+
+    #Update the average of a window
+    def update_average(self, hand, subtract=False):
+        scale = -1 if subtract else 1
+
+        hand_tuple = self.normalize_five_tuple(self.extended_tuple(hand))
+        l = []
+        #string  = str(self.ext_average) + ("-" if subtract else "+") + str(hand_tuple)
+        for (a, b) in zip(self.ext_average, hand_tuple):
+            l.append(a + (scale * b))
+        #print (string + "=" + str(l))
+        self.ext_average = tuple(l)
+
+    #Normalize a five_tuple with capacity as the denominator
+    def normalize_five_tuple(self, tup):
+        l = []
+        for t in tup:
+            l.append(float(t)/self.capacity)
+        return tuple(l)
+
+    #Get a boolean tuple of fingers extended
+    def extended_tuple(self, hand):
+        l = []
+        fse = hand.fingers.extended()
+        for finger in hand.fingers:
+            l.append(finger in fse)
+        return tuple(l)
 
 def main():
     # Create a sample listener and controller
-    listener = JustureListener()
+    listener = JustureListener(10, 20)
     controller = Leap.Controller()
 
     # Have the sample listener receive events from the controller
